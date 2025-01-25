ac121688d4d719b0f09f960abbba5263ce39bc3c
MuriWolf <murillop.o06@gmail.com>
2025-01-25 12:22:16 -0300

    Add revision
    Alguns testes para relembrar o conteúdo já visto.

---
+++ src/estudos/relembrando/basics.java
@@ -0,0 +1,11 @@
+package estudos.relembrando;
+
+record Book(String author, int page) {}
+
+public class basics {
+    public static void main(String[] args) {
+        Book bookOne = new Book("Aldous Huxley", 321);
+
+        System.out.println(bookOne);
+    }
+}
