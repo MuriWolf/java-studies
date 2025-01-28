5dfe91bee07fcfb6c94016a49320eeef5c64defa
MuriWolf <murillop.o06@gmail.com>
2025-01-28 19:03:15 -0300

    Add thread studies

---
+++ src/estudos/threads/domain/Account.java
@@ -0,0 +1,13 @@
+package estudos.threads.domain;
+
+public class Account {
+    private int balance = 50;
+
+    public void withdrawal(int amount) {
+        this.balance = this.balance - amount;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+}
